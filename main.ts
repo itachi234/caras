d1b994fa4de0b6f5432d12dc0433afcef5ec872f
itachi234 <88839257+itachi234@users.noreply.github.com>
2021-09-24 09:01:17 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,41 @@
+function feliz () {
+    basic.showLeds(`
+        # # . # #
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+}
+function enojado () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+}
+function miedo () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        # . . . .
+        . # # # .
+        # . . . #
+        `)
+}
+function triste () {
+    basic.showLeds(`
+        # # . # #
+        . # . # .
+        . . . . .
+        . # # # .
+        # . . . #
+        `)
+}
+basic.forever(function () {
+    if (1 == 0) {
+    	
+    }
+})
